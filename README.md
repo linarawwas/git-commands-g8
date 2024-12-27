ef71861ab8b46d698cb658ff0ca0547aa3aa5c3e
linarawwas <linamkraw2@gmail.com>
2024-12-27 18:51:37 +0200

    my new feature 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## my new feature: 
 this feature is a hello button
 it has a style and an evenlistener on the on click
+# my new feature 2: this will be an evenlistener on our existent div
--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let btn=document.getElementById('mybtn')
 btn.addEventListener('click',()=>{
     btn.style.backgroundColor='red'
 })
+let mydiv=document.querySelector('div')
+mydiv.addEventListener('click', ()=>{
+    mydiv.style.border='1px solid black'
+})
